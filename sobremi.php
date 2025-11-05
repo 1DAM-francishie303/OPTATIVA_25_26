4df55f6e93a4c7bafaffd9181e34ea9d50c88481
unknown <2DAM-fhierid721@IESMARTINEZM.ES>
2025-11-05 19:50:27 +0100

    m

--- sobremi.php
+++ sobremi.php
@@ -6,23 +6,23 @@
     <title>Sobre mi Francisco Hierro</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
+<body class="h-full w-full">
     <?php
-    echo "<h1>SOBRE MÍ</h1>";
+    echo "<h1 class='text-6xl flex justify-center'>SOBRE MÍ</h1>";
     $hobbies = array(
-    array("https://www.deustoformacion.com/sites/deustoformacion/files/imagenes_blog_1/tic/2015/03/source.jpg","ver Youtube","Paso mucho tiempo viendo vídeos de Youtube. Sé que debería dejarlo pero no puedo")
-    ,array("https://i.blogs.es/589313/b1cf7a8695629ac2dd35a72bf2c35f41/500_333.webp","programar","Me encanta programar y es a lo que me quiero dedicar en el futuro. Sobre todo me gusta Java.")
+    array("https://imagenes.elpais.com/resizer/v2/SHLDQW7SLBPYDHIX3YM6OGQ2CQ.jpg?auth=8da182101235684b9475d9be2308f3e046c1a3a374c0bc5aec9c717358708264&width=414","ver Youtube","Paso mucho tiempo viendo vídeos de Youtube. Sé que debería dejarlo pero no puedo")
+    ,array("https://www.misclientesparasiempre.es/wp-content/uploads/2018/01/012018-JavaLenguajeProgramacion.jpg","programar","Me encanta programar y es a lo que me quiero dedicar en el futuro. Sobre todo me gusta Java.")
     ,array("https://st3.depositphotos.com/1000260/16937/i/450/depositphotos_169372520-stock-photo-diet-fat-man-makes-choice.jpg","comer","Me gusta comer mucho. Solo comida de calidad y muy buena como la carne de vaca")
     );
-    
+    echo "<div class='flex flex-row mt-20'>";
     foreach($hobbies as $key => $hobbie) {
-        echo "<div class='border-2 border-solid w-100 h-100'>";
-        echo "<img src=$hobbie[0]/>";
-        echo "<h2>$hobbie[1]</h2>";
+        echo "<div class='border-2 border-solid border-gray-200 rounded-2xl shadow-lg mr-10'>";
+        echo "<img src=$hobbie[0] class='rounded-t-2xl w-full'/>";
+        echo "<h2 class='font-bold'>$hobbie[1]</h2>";
         echo "<p>$hobbie[2]</p>";
         echo "</div>";
     }
-
+    echo "</div>";
     ?>
 </body>
 </html>
