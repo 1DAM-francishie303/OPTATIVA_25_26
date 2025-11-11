cfe151985a94356d708beb64817279f8818db32e
unknown <2DAM-fhierid721@IESMARTINEZM.ES>
2025-11-11 18:59:45 +0100

    m

---
+++ BlogPersonal_FranciscoHierro/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Header</title>
+</head>
+<body>
+     <?php 
+        //HAY QUE HACERLO DE FORMA DINAMICA CON UN ARRAY 
+        //BASICAMENTE COMO SOBRE MI
+
+    ?>
+    <div class="w-screen h-30 bg-blue-400 text-white text-5xl p-3 pt-5">Blog de Francisco Hierro</div>
+    <nav class="w-screen h-10 bg-blue-300">
+        <button class="cursor-pointer hover:text-white m-2 mx-5"><a href="../BlogPersonal_FranciscoHierro/inicio.php">INICIO</a>
+        </button>
+        <button class="cursor-pointer hover:text-white m-2 mx-5"><a href="../BlogPersonal_FranciscoHierro/sobremi.php">SOBRE MÍ</a></button>
+        <button class="cursor-pointer hover:text-white m-2 mx-5"><a href="../BlogPersonal_FranciscoHierro/proyectos.php">PROYECTOS</a></button>
+        <button class="cursor-pointer hover:text-white m-2 mx-5"><a href="../BlogPersonal_FranciscoHierro/contacto.php">CONTACTO</a></button>
+    </nav>
+</body>
+</html>
