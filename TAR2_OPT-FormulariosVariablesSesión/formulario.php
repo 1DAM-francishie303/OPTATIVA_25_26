01e49c83183b3b3aab7cac15820ccdba8aa72bdc
unknown <2DAM-fhierid721@IESMARTINEZM.ES>
2025-11-19 18:14:32 +0100

    m

--- TAR2_OPT-FormulariosVariablesSesión/formulario.php
+++ TAR2_OPT-FormulariosVariablesSesión/formulario.php
@@ -7,22 +7,19 @@
   </head>
   <body class="w-full h-full bg-gray-200">
     <?php
-        session_start();
-        if(isset($_POST['nombre'])){
-            $_SESSION['nombre'] = $_POST['nombre'];
-            $nombre = $_SESSION['nombre'];
+       session_start();
+       $nombre = $_SESSION['nombre'] ?? "";
+       $apellido = $_SESSION['apellido'] ?? "";
+       
+        if(isset($_POST["limpiar"])){
+          session_unset(); //Borramos todos los datos de la sesión si limpia
+          $nombre = "";
+          $apellido = "";
         }
-        if(isset($_POST['apellido'])){
-            $_SESSION['apellido'] = $_POST['apellido'];
-            $apellido = $_SESSION['apellido'];
-          }
-         if(isset($_POST["limpiar"])){
-            session_unset(); //Borramos todos los datos de la sesión si limpia
-            $nombre = "";
-            $apellido = "";
-         }
+        
+        
     ?>
-    <form class="border-2 h-170 border-gray-300 shadow-2xl w-100 m-10 rounded-2xl bg-gray-100" method="POST">
+    <form class="border-2 h-170 border-gray-300 shadow-2xl w-100 m-10 rounded-2xl bg-gray-100" method="POST" action="procesar.php">
       <div class="flex flex-col ml-10 mt-7">
         <label for="nombre">Nombre</label>
         <input type="text" value="<?= $nombre ?>" name="nombre" class="w-2/3 border-2 rounded-lg border-gray-300">
@@ -31,17 +28,17 @@
         <label for="correo">Correo electrónico</label>
         <input type="text" name="correo" class="w-2/3 border-2 rounded-lg border-gray-300">
         <select name="paises" id="1" class="mt-8 w-2/3 border-2 rounded-lg border-gray-300">
-          <option value="">Selecciona tu país</option>
-          <option value="1">España</option>
-          <option value="2">Francia</option>
-          <option value="3">Italia</option>
+          <option value="Ningun pais">Selecciona tu país</option>
+          <option value="España">España</option>
+          <option value="Francia">Francia</option>
+          <option value="Italia">Italia</option>
         </select>
         <p class="mt-7">Selecciona tus intereses</p>
-        <select multiple name="intereses" id="2" class="w-2/3 border-2 rounded-lg border-gray-300 p-2">
-          <option value="1">Tecnología</option>
-          <option value="2">Economía</option>
-          <option value="3">Arte</option>
-          <option value="4">Deportes</option>
+        <select multiple name="intereses[]" id="2" class="w-2/3 border-2 rounded-lg border-gray-300 p-2"> <!--PARA QUE TE COJA LAS OPCIONES COMO UN ARRAY DEBES PONER EN EL NOMBRE CORCHETES []-->
+          <option value="Tecnología">Tecnología</option>
+          <option value="Economía">Economía</option>
+          <option value="Arte">Arte</option>
+          <option value="Deportes">Deportes</option>
         </select>
         <div class="mt-5">
           <input type="radio" name="comprobarEdad">
@@ -53,26 +50,26 @@
         </div>
 
         <div>
-          <input type="checkbox" id="vs" name="herramientas" value="vs" class="mt-5">
+          <input value="VS Code" type="checkbox" id="vs" name="herramientas[]" value="vs" class="mt-5">
           <label for="vs">VS Code</label>
         </div>
         <div>
-          <input type="checkbox" id="in" name="herramientas" value="in">
+          <input value="IntelliJ" type="checkbox" id="in" name="herramientas[]" value="in">
           <label for="in">IntelliJ</label>
         </div>
         <div>
-          <input type="checkbox" id="net" name="herramientas" value="net">
+          <input value="NetBeans" type="checkbox" id="net" name="herramientas[]" value="net">
           <label for="net">NetBeans</label>
         </div>
         <div>
-            <input type="checkbox" id="blue" name="herramientas" value="blue">
+            <input value="BlueJ" type="checkbox" id="blue" name="herramientas[]" value="blue">
             <label for="blue">BlueJ</label>
         </div>
         </div>
-
-        <button name="" action="procesar.php" type="submit" class="ml-10 mt-5 bg-green-400 px-3 py-1 border-2 cursor-pointer">Enviar</button>
         
-        <button name="limpiar" action="" type="submit" class="ml-15 mt-5 bg-red-400 px-3 py-1 border-2 cursor-pointer">Limpiar</button>
+        <button type="submit" class="ml-10 mt-5 bg-green-400 px-3 py-1 border-2 cursor-pointer">Enviar</button>
+        
+        <button  type="submit" formaction="" name="limpiar" class="ml-15 mt-5 bg-red-400 px-3 py-1 border-2 cursor-pointer">Limpiar</button>
 
     </form>
 
--- TAR2_OPT-FormulariosVariablesSesión/procesar.php
+++ TAR2_OPT-FormulariosVariablesSesión/procesar.php
@@ -3,24 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script><!--ESTO ES PARA IMPORTAR TAILWINDCSS EN REMOTO-->
     <title>Procesas Francisco Hierro</title>
 </head>
 <body>
     <?php
-        if(!isset($_POST["nombre"])){
+    session_start();
+    $_SESSION['nombre'] = $_POST['nombre'] ?? ""; //Esto es como un operador ternario ? pero sin necesidad de hacer el isset()
+    $_SESSION['apellido'] = $_POST['apellido'] ?? "";
+
+        if(empty($_POST["nombre"])){ //Si usara !isset() en vez de empty estaría diciendo si es null pero siempre no es null ya que si no pongo nada es "" 
             echo "<p>Nombre vacio, rellénalo</p>";
         }
-         if(!isset($_POST['apellido'])){
+         if(empty($_POST['apellido'])){
             echo "<p>Apellidos vacio, rellénalo</p>";
         }
-        if(isset($_POST['correo'])){
-            if (filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL)) {
-                echo "Correo válido";
-            } else {
-                echo "Correo inválido";
-            }
+        
+        if (filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL)) {
+            echo "Correo válido"."<br>";
+        } else {
+            echo "Correo inválido"."<br>";
         }
 
+        /*
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+        */
+        echo "<div class='border-t-2 w-50 mt-10'>TUS DATOS<div>";
+        
+        echo $_POST["nombre"]."<br>"; //Para que no imprima nada si es null podriamos hacer echo isset($_POST['nombre']) ? $_POST['nombre'] : '';
+
+        echo $_POST['apellido']."<br>";
+        echo $_POST['correo']."<br>";
+        echo $_POST['paises']."<br>";
+
+        //Una forma para IMPRIMIR ARRAYS
+        if (!empty($_POST['intereses']) && is_array($_POST['intereses'])) {
+            echo count($_POST['intereses'])." intereses.";
+            echo "Tus intereses: " . implode(", ", $_POST['intereses'])."<br>";
+        } else {
+            echo "No seleccionaste ningún interés"."<br>";
+        }
+
+        //OTRA FORMA MAS COMPLETA DE HACER EL ECHO: Si no es true imprime la variable del array pero si es null imprime vacio
+        if (!empty($_POST['herramientas']) && is_array($_POST['herramientas'])) {
+            echo implode(", ", $_POST['herramientas']);
+        } else {
+            echo "No seleccionaste ninguna herramienta";
+        }
+
+
     ?>
+    <br>
+    <button class="px-3 py-1 bg-green-400 border-2"><a href="formulario.php">Volver</a></button>
 </body>
 </html>
